f5837b0087a558ebad524d6cd3e4b848e66dc2e1
Justin Hunter <justin@pinata.cloud>
2025-05-13 09:19:44 -0500

    added try/catch around redirect parsing to prevent errors from being thrown during upload

--- src/utils/redirects.ts
+++ src/utils/redirects.ts
@@ -1,24 +1,31 @@
 export const parseRedirectsFile = async (content: string) => {
   const rules = [];
+  try {
+    const lines = content.split("\n");
 
-  const lines = content.split("\n");
-
-  for (const line of lines) {
-    if (line.trim().startsWith("#") || line.trim() === "") continue;
-
-    const parts = line.trim().split(/\s+/);
-    if (parts.length < 2) continue;
-
-    const rule = {
-      source: parts[0],
-      destination: parts[1],
-      status: parts.length > 2 ? parseInt(parts[2], 10) : 301,
-      force: parts.includes("!"),
-      proxy: parts.includes("200"),
-    };
-
-    rules.push(rule);
-  }
+    for (const line of lines) {
+      try {
+        if (line.trim().startsWith("#") || line.trim() === "") continue;
+  
+        const parts = line.trim().split(/\s+/);
+        if (parts.length < 2) continue;
+    
+        const rule = {
+          source: parts[0],
+          destination: parts[1],
+          status: parts.length > 2 ? parseInt(parts[2], 10) : 301,
+          force: parts.includes("!"),
+          // proxy: parts.includes("200"),
+        };
+    
+        rules.push(rule); 
+      } catch (error) {
+        console.log(error);
+      }    
+    } 
+  } catch (error) {
+    console.log(error);    
+  }  
 
   return rules;
 }
